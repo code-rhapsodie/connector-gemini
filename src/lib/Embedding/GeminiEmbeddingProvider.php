acdde31573851fb7f8c50cba1bef4c0e8d8bc9f7
loic <loic@code-rhapsodie.fr>
2026-02-17 14:24:40 +0100

    add taxonomy suggestion for embedding providers

---
+++ src/lib/Embedding/GeminiEmbeddingProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace CodeRhapsodie\ConnectorGemini\Embedding;
+
+use CodeRhapsodie\Contracts\ConnectorGemini\ClientProviderInterface;
+use Ibexa\Contracts\Core\Search\Embedding\EmbeddingConfigurationInterface;
+use Ibexa\Contracts\Core\Search\Embedding\EmbeddingProviderInterface;
+use RuntimeException;
+
+final class GeminiEmbeddingProvider implements EmbeddingProviderInterface
+{
+    private ClientProviderInterface $clientProvider;
+
+    private EmbeddingConfigurationInterface $embeddingConfiguration;
+
+    public function __construct(
+        ClientProviderInterface $clientProvider,
+        EmbeddingConfigurationInterface $embeddingConfiguration
+    ) {
+        $this->clientProvider = $clientProvider;
+        $this->embeddingConfiguration = $embeddingConfiguration;
+    }
+
+    /**
+     * @return float[]
+     *
+     * @throws \JsonException
+     */
+    public function getEmbedding(string $content, ?string $model = null, ?int $maxTokens = null): array
+    {
+        return $this->clientProvider->getClient()->embeddingModel($model ?? $this->embeddingConfiguration->getDefaultModel()['name'])->embedContent($content)->embedding->values;
+    }
+}
